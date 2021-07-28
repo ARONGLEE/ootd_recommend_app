c0433e9652b1b5d1174e4cb385b97efa9b932f5c
ARONGLEE <arong16@naver.com>
2021-07-28 14:12:34 +0900

    weather
    city, weather, temp

--- js/weather.js
+++ js/weather.js
@@ -3,9 +3,15 @@ const API_KEY = "3b4fa3ed7ba9793ead0dc8bc71c770e4";
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=37.496981999999996&lon=126.5553759&appid=${API_KEY}`
+    const lon = position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        city.innerText = data.name; 
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.");
